40e0a9161eb2f65c7bd959ab68d9dc1ee1509eef
Siigismund <lahdenperamiika@gmail.com>
2022-11-17 11:11:31 +0200

    Update toyClassInheritance.java
    Added new class "Toy"

--- toyClassInheritance.java
+++ toyClassInheritance.java
@@ -2,8 +2,36 @@
 public class toyClassInheritance {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+	} 
+
+} //end of main
+
+class Toy{
+	
+	public String toyName = "";
+	public int toyWeight = 0;
+	
+	//default constructor
+	public Toy() 
+	{
+		toyName = "";
+		toyWeight = 0;
 	}
-
+	
+	//parameterized constructor
+	public Toy(String name, int weight) 
+	{
+		toyName = name;
+		toyWeight = weight;
+	}
+	
+	public void printData()
+	{
+		System.out.println("Name: " + toyName);
+		System.out.println("Weight: " + toyWeight);
+	}
+	
 }
+	
