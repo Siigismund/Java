eb2b914dadac413d7972b297d3941ed3c7f1b658
Siigismund <lahdenperamiika@gmail.com>
2022-11-18 10:45:29 +0200

    Update toyClassInheritance.java
    Added subclass Lighsaber

--- toyClassInheritance.java
+++ toyClassInheritance.java
@@ -3,14 +3,19 @@ public class toyClassInheritance {
 
 	public static void main(String[] args) {
 		
-
+		Toy toy = new Toy("Car", 5);
+		toy.printData();
+		
+		Lightsaber lightsaber = new Lightsaber("Lightsaber", 5);
+		lightsaber.printData();
+		
 	} 
 
 } //end of main
 
 class Toy{
 	
-	public String toyName = "";
+	protected String toyName = "";
 	public int toyWeight = 0;
 	
 	//default constructor
@@ -33,5 +38,38 @@ class Toy{
 		System.out.println("Weight: " + toyWeight);
 	}
 	
-}
+} //end of Toy
 	
+class Lightsaber extends Toy{
+	
+	public boolean isOn;
+	
+	//default constructor
+	public Lightsaber() 
+	{
+		toyName = "";
+		toyWeight = 0;
+	}
+	
+	//parameterized constructor
+	public Lightsaber(String name, int weight) 
+	{
+		toyName = name;
+		toyWeight = weight;
+	}
+		
+	public void turnOn() 
+	{
+		isOn = true;
+	}
+	
+	@Override
+	public void printData()
+	{
+		turnOn();
+		System.out.println("Name: " + toyName);
+		System.out.println("Weight: " + toyWeight);
+		System.out.println("Is on: " + isOn);
+	}
+	
+} //end of Lightsaber
